b8945ddcf8d8d23d935c848d60721b56e18448bb
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-07-12 20:49:53 +0300

    [FEATURE] updated css

--- components/Destinations.js
+++ components/Destinations.js
@@ -2,28 +2,19 @@ import styles from '../styles/Home.module.css';
 import { DCard } from './DCard';
 
 export const Destination = () => {
+    const numberOfCards = 4;
+
+    const cardsArray = new Array(numberOfCards).fill(null);
+
     return (
         <div className={styles.destination}>
             <h1>Packages</h1>
         <div className={styles.destinationwrapper}>
-        <div className={styles.cardwrapper}>
-            <DCard/>
+        {cardsArray.map((_, index) => (
+        <div key={index} className={styles.cardwrapper}>
+          <DCard />
         </div>
-        <div className={styles.cardwrapper}>
-            <DCard/>
-        </div>
-        <div className={styles.cardwrapper}>
-            <DCard/>
-        </div>
-        <div className={styles.cardwrapper}>
-            <DCard/>
-        </div>
-        <div className={styles.cardwrapper}>
-            <DCard/>
-        </div>
-        <div className={styles.cardwrapper}>
-            <DCard/>
-        </div>
+      ))}
         </div>
         </div>
     );
--- components/Services.js
+++ components/Services.js
@@ -2,19 +2,18 @@ import styles from '../styles/Home.module.css';
 import { SCard } from './SCard';
 
 export const Services = () => {
+    const numberOfCards = 4;
+
+    const cardsArray = new Array(numberOfCards).fill(null);
     return (
         <div className={styles.services}>
             <h1>Services Offered</h1>
         <div className={styles.serviceswrapper}>
-        <div className={styles.cardwrapper}>
-            <SCard/>
+        {cardsArray.map((_, index) => (
+        <div key={index} className={styles.cardwrapper}>
+          <SCard />
         </div>
-        <div className={styles.cardwrapper}>
-            <SCard/>
-        </div>
-        <div className={styles.cardwrapper}>
-            <SCard/>
-        </div>
+      ))}
         </div>
         </div>
     );
